bd95215c84488cfdc2ea49e9e3fdf067db7a63d8
ljy5490 <ljy5490@foxmail.com>
2020-07-28 09:25:45 +0100

    first commit

---
+++ tweets/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+
+from .models import Tweet
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{'id': x.id, 'content':x.conetent} for x in qs]
+    data = {
+        'response': tweets_list
+    }
+    return JsonResponse(data)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    
+    data = {
+        'id': tweet_id,
+    }
+    status = 200
+
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status)
+    #return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
