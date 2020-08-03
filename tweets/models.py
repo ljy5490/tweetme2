e0e33fea1c98167a28169c5b55cea7d821584951
ljy5490 <ljy5490@foxmail.com>
2020-08-03 09:22:40 +0100

    Backup commit

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,38 @@
+import random
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
+
 # Create your models here.
+
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
+
+    parent = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    @property
+    def is_retweet(self):
+        return self.parent != None
+
+    # def serialize(self):
+    #     return {
+    #         'id': self.id,
+    #         'content': self.content,
+    #         'likes': random.randint(0, 200)
+    #     }
+
+    def __str__(self):
+        return self.content
